dd3ff1e9ba8a466997cbcec3dfde713357d284a6
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2017-05-25 16:34:25 +0100

    NGC-1878 removed typo

--- app/uk/gov/hmrc/pushnotification/domain/PushMessageResponse.scala
+++ app/uk/gov/hmrc/pushnotification/domain/PushMessageResponse.scala
@@ -5,6 +5,6 @@ import play.api.libs.json.Json
 case class PushMessageResponse(messages: Seq[PushMessage])
 
 object PushMessageResponse {
-  implicit val formatPushMessage = Json.format[PushMessagex]
+  implicit val formatPushMessage = Json.format[PushMessage]
   implicit val format = Json.format[PushMessageResponse]
 }
